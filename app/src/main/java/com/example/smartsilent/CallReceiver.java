225553bd6fba1eafd6a93bb42812dcc7d6321764
nicolae1099 <nitu.nicolae.1097@gmail.com>
2020-08-24 00:08:54 +0300

    am adaugat CallReceiver si optiunea de a vedea numarul care apeleaza.+ permisiunile necesare in Manifest

---
+++ app/src/main/java/com/example/smartsilent/CallReceiver.java
@@ -0,0 +1,80 @@
+package com.example.smartsilent;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.Toast;
+
+public class CallReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //Log.w("intent " , intent.getAction().toString());
+        TelephonyManager telephony = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+        MyPhoneStateListener customPhoneListener = new MyPhoneStateListener();
+
+        telephony.listen(customPhoneListener, PhoneStateListener.LISTEN_CALL_STATE);
+
+        Bundle bundle = intent.getExtras();
+        String phone_number = bundle.getString("incoming_number");
+        System.out.println("phone number isssssss " + phone_number);
+
+        String stateStr = intent.getExtras().getString(TelephonyManager.EXTRA_STATE);
+        // String number = intent.getExtras().getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+        int state = 0;
+        if(stateStr.equals(TelephonyManager.EXTRA_STATE_IDLE)){
+            state = TelephonyManager.CALL_STATE_IDLE;
+        }
+        else if(stateStr.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)){
+            state = TelephonyManager.CALL_STATE_OFFHOOK;
+        }
+        else if(stateStr.equals(TelephonyManager.EXTRA_STATE_RINGING)){
+            state = TelephonyManager.CALL_STATE_RINGING;
+        }
+        if (phone_number == null || "".equals(phone_number)) {
+            return;
+        }
+        //customPhoneListener.onCallStateChanged(context, state, phone_number);
+        Toast.makeText(context, "Phone Number " + phone_number , Toast.LENGTH_SHORT).show();
+    }
+}
+
+/*import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+
+
+public class CallReceiver extends BroadcastReceiver {
+    private final static String TAG = "Tagg";
+    @Override
+    public void onReceive(Context arg0, Intent arg1) {
+
+        if(arg1.getAction().equals("android.intent.action.PHONE_STATE")){
+
+            String state = arg1.getStringExtra(TelephonyManager.EXTRA_STATE);
+
+            if(state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)){
+                Log.d(TAG, "Inside Extra state off hook");
+                //String number = arg1.getStringExtra(TelephonyManager.EXTRA_PHONE_NUMBER);
+                //Log.e(TAG, "outgoing number : " + number);
+            }
+
+            else if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)){
+                Log.e(TAG, "Inside EXTRA_STATE_RINGING");
+                String number = arg1.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+                Log.e(TAG, "incoming number : " + number);
+            }
+            else if(state.equals(TelephonyManager.EXTRA_STATE_IDLE)){
+                Log.d(TAG, "Inside EXTRA_STATE_IDLE");
+            }
+        }
+    }
+}
+*/
--- app/src/main/java/com/example/smartsilent/MainActivity.java
+++ app/src/main/java/com/example/smartsilent/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.smartsilent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.AudioManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -12,12 +16,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static android.media.AudioManager.ADJUST_RAISE;
 
 public class MainActivity extends AppCompatActivity{
 
     ImageButton addProfile;
     AudioManager audioManager;
+    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,15 +37,11 @@ public class MainActivity extends AppCompatActivity{
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-            if (!notificationManager.isNotificationPolicyAccessGranted()) {
+        if (!notificationManager.isNotificationPolicyAccessGranted()) {
 
-                Intent intent = new Intent(
-                        android.provider.Settings
-                                .ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
-
-                startActivity(intent);
-            }
-
+            Intent intent = new Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+            startActivity(intent);
+        }
         addProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -47,7 +52,47 @@ public class MainActivity extends AppCompatActivity{
                 startActivity(intent);
             }
         });
-
+        checkAndRequestPermissions();
 
     }
+
+    private  boolean checkAndRequestPermissions() {
+
+        int readPhoneState = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE);
+        int read_call_log = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG);
+        List listPermissionsNeeded = new ArrayList<>();
+        if (readPhoneState != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.READ_PHONE_STATE);
+        }
+
+        if (read_call_log != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.READ_CALL_LOG);
+        }
+
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this,
+                    (String[]) listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]),
+                    REQUEST_ID_MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
+    }
 }
+
+/*
+        if (getApplicationContext().checkSelfPermission(Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission has not been granted, therefore prompt the user to grant permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_PHONE_STATE}, REQUEST_ID_MULTIPLE_PERMISSIONS);
+        }
+
+        if (getApplicationContext().checkSelfPermission(Manifest.permission.PROCESS_OUTGOING_CALLS)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission has not been granted, therefore prompt the user to grant permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.PROCESS_OUTGOING_CALLS},
+                    REQUEST_ID_MULTIPLE_PERMISSIONS);
+        }
+
+ */
