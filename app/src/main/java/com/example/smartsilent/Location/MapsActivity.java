e81a11c4b1b83a60c025d23db106c02e9cd85b75
nicolae1099 <nitu.nicolae.1097@gmail.com>
2020-08-25 20:18:04 +0300

    added location maps and markers on maps v2

---
+++ app/src/main/java/com/example/smartsilent/Location/MapsActivity.java
@@ -0,0 +1,143 @@
+package com.example.smartsilent.Location;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import com.example.smartsilent.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap map;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+
+                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                centreMapOnLocation(lastKnownLocation,"Your Location");
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+        map = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng bucharest = new LatLng(44.456718, 26.044516);
+        map.addMarker(new MarkerOptions().position(bucharest).title("Home"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(bucharest));
+
+        // Setting a click event handler for the map
+        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                // Creating a marker
+                MarkerOptions markerOptions = new MarkerOptions();
+
+                // Setting the position for the marker
+                markerOptions.position(latLng);
+
+                // Setting the title for the marker.
+                // This will be displayed on taping the marker
+                markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+
+                // Clears the previously touched position
+                map.clear();
+
+                // Animating to the touched position
+                map.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                // Placing a marker on the touched position
+                map.addMarker(markerOptions);
+            }
+        });
+
+        map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                String adress ="";
+                try {
+                    List<Address> listaddress = geocoder.getFromLocation(latLng.latitude, latLng.longitude,1);
+
+                    if (listaddress != null && listaddress.size()>0){
+                        if (listaddress.get(0).getThoroughfare() != null){
+
+                            if (listaddress.get(0).getSubThoroughfare() != null){
+                                adress += listaddress.get(0).getSubThoroughfare() + "";
+
+                            }
+                            adress += listaddress.get(0).getThoroughfare();
+                        }
+
+                    }
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                map.clear();
+                map.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                map.addMarker(new MarkerOptions().position(latLng).title(adress));
+            }
+        });
+    }
+
+    public void centreMapOnLocation(Location location, String title){
+
+        LatLng userLocation = new LatLng(location.getLatitude(),location.getLongitude());
+        map.clear();
+        map.addMarker(new MarkerOptions().position(userLocation).title(title));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,12));
+
+    }
+}
