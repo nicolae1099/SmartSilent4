f14ebc37acce3158dee00438b6dfc5e08eba4bd8
nicolae1099 <nitu.nicolae.1097@gmail.com>
2021-03-21 13:59:37 +0200

    added MaterialComponents for MakeTimeZone

--- app/src/main/java/com/example/smartsilent/Room/SilencePreferencesDatabase.java
+++ app/src/main/java/com/example/smartsilent/Room/SilencePreferencesDatabase.java
@@ -9,7 +9,7 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
-@Database(entities = {Contact.class}, version = 1)
+@Database(entities = {Contact.class}, version = 1, exportSchema = false)
 public abstract class SilencePreferencesDatabase extends RoomDatabase {
 
     private static SilencePreferencesDatabase instance;
